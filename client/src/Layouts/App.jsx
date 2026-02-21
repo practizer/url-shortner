1b1970ee0747a5df4a091450c6d2b631b3ec58e8
jaisondavid-m <jaison7373@gmail.com>
2026-02-21 19:56:41 +0530

    fixed some errs

--- client/src/Layouts/App.jsx
+++ client/src/Layouts/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
